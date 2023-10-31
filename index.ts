365674df4a939762e84ec338f37047853b762af1
Mariam Anwar <mariam.anwar478@gmail.com>
2023-10-31 23:08:24 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,555 @@
+//question No.2
+let personname:string=("Ibraheem")
+console.log(`hello Eric,would you like to learn some python today? ${personname}`)
+
+//Question no.3
+let personnamee:string=(" muhammad ismaeel aizaz").toUpperCase()
+console.log(personnamee)
+
+let apersonname:string=("Aizaz Anwar").toLowerCase()
+console.log(apersonname)
+
+
+//titlecase
+
+
+let words: string[] = personnamee.split("  ");
+let titlecasename: string =""
+for(let i = 0 ; i < words.length; i++) {
+titlecasename +=words[i].charAt(0).toUpperCase() +words[i].slice(1).toLowerCase()+" "
+};
+console.log(titlecasename)
+
+//question no.4
+let quote:string='"Have good hopes but prepare according to bad circumstances."'
+console.log(`once Quaid said,${quote}`);
+
+//question n0.5
+let perrsonname:string ="Imran khan"
+let message:string= perrsonname  +  "ap ne gahbrana ni hai"
+console.log(message)
+
+//question no.6
+
+let aname:string="\t  moiz  \n"
+
+console.log(aname)
+
+let strippedname:string=aname.trim()
+console.log(strippedname)
+
+//questuon no.7
+
+function minfunc(a:number,b:number){
+  console.log(a-b) 
+}
+minfunc(16,8)
+
+function addfunc(d:number, e:number){
+  console.log(d+e)
+}
+addfunc(4,4)
+ 
+function multiplication(q:number,r:number){console.log(
+  q*r
+)}
+multiplication(4,2)
+
+function division(s:number,p:number){console.log(
+  s/p)}
+division(16,2)
+
+// question no.8
+console.log("-".repeat(10))
+console.log(5+3)
+console.log("-".repeat(10))
+console.log(5+3)
+console.log("-".repeat(10))
+console.log(5+3)
+console.log("-".repeat(10))
+
+//qestion no.9
+let favoritenum:number=7
+console.log("my favorite number is " + favoritenum)
+
+//question no.10
+
+//typescript// PIAIC President initiative artificial computing.
+
+//question no.11
+let friend1:string="Ayesha"
+let friend2:string="Alisha"
+let friend3:string="Umaira"
+let friendsarray=[friend1 , friend2  , friend3]
+
+console.log(friendsarray[0])
+console.log(friendsarray[1])
+console.log(friendsarray[2])
+
+//Question no.12
+friendsarray.forEach((friendsname)=>{
+  console.log(`Have you done your assignment ${friendsname}`)
+})
+
+//Question no.13
+let favtrans:Array<[trans:string,brand:string]>=[]
+
+favtrans.push(["motocycle","honda"])
+favtrans.push(["car","audi"])
+favtrans.push(["car1","corolla"])
+
+favtrans.forEach(([transport,brand]) => {
+  console.log(`i would like to own a ${brand} ${transport}`)})
+
+//question no.14
+let guestlist=["Mian.Muhammad Anwar", "Aizaz Anwar", "M.Ibraheem Aizaz"];
+
+guestlist.forEach((guestname) =>{
+  console.log(`A.A!hope you are fine ${guestname} you are invited to a dinner`)
+})
+//question n0.15
+
+guestlist.splice(2,1,"M.ismaeel")
+console.log(guestlist)
+
+guestlist.forEach((guestnamee)=>{
+  console.log(`you are invited to a dinner ${guestnamee}`)
+})
+//question no.16
+//1st step
+console.log(`we have found a biger dinner table ${guestlist}`)
+
+//2nd step 
+guestlist.unshift("yahya")
+console.log(guestlist)
+
+//3rd step
+guestlist.splice(2,0,"hamza")
+console.log(guestlist)
+
+//4th step
+let newguestlist:string="Shakeel"
+guestlist.push(newguestlist)
+console.log(guestlist)
+
+//5th step
+guestlist.forEach((finalguest)=>{
+  console.log(`A.A !${finalguest} we've arranged a  dinner.it will be a great pleasure if you join us at dinner.`)
+})
+
+//question no.17
+
+//step 1
+console.log("we can invite only 2 people for dinner")
+
+//step 2
+while (guestlist.length>2)
+{let removeguest1=guestlist.pop()
+
+console.log(`sorry ${removeguest1} i can invite only two person`)}
+//step3
+console.log(`you are still invited ${guestlist}`)
+
+//step4
+
+guestlist.pop()
+guestlist.pop()
+console.log(`guestlist after removing last 2 persons: ${guestlist}`)
+
+//question no.18
+let placesaraay=["hunza valley" ,"neelum valley" , "sawat valley" , "attabad lake"]
+
+//print the array
+console.log(placesaraay)
+
+//Aplhabatic order
+
+let sortarray=placesaraay
+sortarray.sort()
+console.log(sortarray)
+//original form
+let oplaces=placesaraay
+console.log(oplaces)
+
+
+//question #19
+let age : number=26
+if (age<2){
+  console.log ("the person isa baby");
+} else if(age <4){
+  console.log("the person is a toddler")
+}else if(age <13){
+  console.log("the person is a kid")
+}else if(age<20){console.log("the person is a teenager"
+)}
+ else  if(age<65){console.log("ther person is a adult")}
+else{
+  console.log("the person is elder ")}
+
+  //question no.20
+  const lanuages:string[]=["Urdu","English","Spsnich", "French","japanese"]
+  for(let language of lanuages){
+    console.log(language)
+  }
+
+  //Question#21
+
+  //question no.22
+  function mobiles(name:string,model:string){
+    return{
+      name,
+      model,
+    
+    };
+  }
+const mymobiles=[
+ mobiles("techno Pova Neo","techno LE6"),
+ mobiles("huawei p10 Lite", "WAS-LX1")
+]
+const invalidindex=3
+
+//question no.23
+
+let car='subaru';
+console.log(" is car=='sabaru'? i predict it true.")
+console.log(car=='subaru')
+
+console.log("is car !='Subaru'?i predict it true")
+console.log(car!='Subaru')
+
+console.log("is car != 'honda'? i predit it true")
+console.log(car !='honda')
+
+console.log("is car ==='subaru'?i predict it true")
+console.log(car==='subaru')
+
+console.log("is car!='subaru'?i predict it false")
+console.log(car!='subaru')
+
+console.log("is car =='civic'?i prdict it fasle")
+console.log(car=='civic')
+
+console.log("is car =='toyota'?i predict it false")
+console.log(car=='toyota')
+
+let score=50
+console.log("is score>55? i predict it false")
+console.log(score>55)
+
+console.log("is score!=50 ?i predict it false")
+console.log(score!=50)
+
+console.log("is score==50? i predict it true")
+console.log(score==50)
+
+//question no.24
+
+//test for equailty and inequailty in strings
+let word1="Assignment"
+let word2="assignment"
+
+console.log("is word1==word2 ?")//false
+console.log(word1==word2)
+
+console.log("is word1!=word2?")//true
+console.log(word1!=word2)
+
+//test using the lowercase function
+let myname="Mariam"
+let yourname="mariam"
+console.log(myname==yourname)//false
+console.log(myname.toLowerCase()==yourname)//true
+
+//numerical test involving equailty and inequailty
+let digit=50
+let digit1=60
+console.log(digit!=digit1)//true
+console.log(digit==digit1)//false
+
+//great than and less than
+console.log(digit>digit1)//false
+console.log(digit<digit1)//true
+
+//greater than or equal to  and less than or equal to
+
+console.log(digit >= digit1)//false
+console.log(digit<=digit1)//true
+
+//using 'and' and 'or' operaters
+let s=10
+let t=15
+let u=20
+console.log(s<t && u>t)//true
+console.log(s>t && u<t)//false
+
+console.log(s<t || u>t)//true
+console.log(s>t|| u<t)//false
+
+// wether item is in an array
+let subjects=["english", "math","biology"]
+console.log(subjects.constructor ===Array)
+
+//wether item is not in the array
+let subject="chemistry"
+console.log(subject.constructor===Array)
+
+//question no.25
+let alien_colour="green"
+if(alien_colour == 'green')
+{console.log("congratulations ! you have earned 5 points")}
+
+ 
+if(alien_colour !== 'green'){
+console.log("congratulations ! you have earned 5 points")}
+
+//question no.26
+let allien_colour='green'
+
+if(allien_colour== 'green'){console.log
+  "then player just earned 5 points"
+}else{
+console.log("the player has just got 10 points")}
+
+if(allien_colour=='blue'){console.log("the player has got 15 points ")
+}else(console.log("the player has got 20 points"))
+
+//question no.27
+let allienn_colour:string='green'
+if(allienn_colour==='green'){
+  console.log("the player has got 5 points")}
+
+else if(allienn_colour==='yellow'){
+  console.log("the player has got 10 points")}
+
+else if(allienn_colour==='red'){"yhe player has got 15 points"}
+else(console.log("what is the actual colour of allien "))
+
+//for yellow colour
+allienn_colour='yellow'
+if(allienn_colour==='green'){
+  console.log("the player has got 5 points")}
+
+else if(allienn_colour==='yellow'){
+  console.log("the player has got 10 points")}
+
+else if(allienn_colour==='red'){"yhe player has got 15 points"}
+else(console.log("what is the actual colour of allien "))
+
+//for red colour
+allienn_colour='red'
+if(allienn_colour==='green'){
+  console.log("the player has got 5 points")}
+
+else if(allienn_colour==='yellow'){
+  console.log("the player has got 10 points")}
+
+else if(allienn_colour==='red'){console.log("yhe player has got 15 points")}
+
+else(console.log("what is the actual colour of allien "))
+
+//question no.28
+
+let aage=26
+if (aage<2){console.log("the person is baby")}
+
+else if(aage<4){console.log("the person ia atoddler")}
+
+else if(aage<13){console.log("the person ia a kid")}
+
+ else if(aage<20){console.log("the person is a teenager")}
+
+ else if(age <65){console.log("the person ia an adult")}
+ 
+ else ("the person in an elder")
+
+ //question no.29
+ 
+
+ //question no.30
+ const usernames:string[]=["Aqsa" , "Fazia", "Samia", "Amina", "Admin"]
+
+function greetusers(){
+  for (const i in usernames)
+{
+if(usernames[i]=== "Admin"){console.log("hello Admin ,would you like to see a status report")}
+
+else{console.log(`hello ${usernames[i]},thankyou for logging in again.`)}}}
+greetusers()
+
+//Question no.31
+
+
+//Question no.32
+
+
+//Question no.33
+const numbers: number[] =[1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+for (const number of numbers) {
+  let ordinal:string;
+
+  if(number===1) {
+     ordinal= "st";
+  } else if (number===2){
+    ordinal="nd";
+  }
+else if(number ===3){
+  ordinal="rd";
+  } else{
+    ordinal="th";}
+
+console.log(`${number}${ordinal}`);
+}
+//Qustion no.34
+
+const pizzas:string[]=[ "periperi" ,"chicken supreme" ,"chicken tikka"]
+for(let i=0; i <pizzas.length; i++){
+  console.log(pizzas[i])
+}
+for (let i=0; i< pizzas.length; i++ ){
+console.log(`i like ${pizzas[i]} pizza`)
+}
+//Question no.35
+const animals:string[]=["Rabbit","dog","cat"]
+for(const animal of animals){
+  console.log(animal)
+}
+for(const animal of animals){
+    if (animal === 'dog'){
+        console.log(`a ${animal} is a good pet`)
+    }
+    else if(
+        animal==='Rabbit'){
+            console.log( `a ${animal} is a better pet`)
+        }
+        else if(animal==='cat'){console.log(`a ${animal} is also a good pet`)}
+}
+console.log("what these animals have in common") ;
+console.log("Any of these animals would make a great pet")
+
+//Question no.36
+const make_shirt=(size:string,message:string):void=>{
+  console.log(`size:${size},message:'${message}'`);
+
+}
+//call the function 
+let size:string="medium";
+let Message:string="nothing to say"
+
+make_shirt(size,Message);
+make_shirt("large","nothing to say");
+
+//question no.37
+function makeShirt(size:string="large",messagee:string="do you love typescript?"):void
+{
+  return console.log(`size:${size},messagee:'${messagee}'`)
+}
+
+makeShirt();
+
+makeShirt("medium");
+
+makeShirt("small","hi");
+
+//Question no.38
+const  city_Country=(city:string , country:string = 'unknown country')=>{
+    console.log(`${city } is in ${country}`)
+}
+city_Country("Gujranwala" , "Pakistan");
+city_Country("Bombay", "India")
+city_Country("Denmark")
+
+//Question no.39
+const city_country=(city:string, country:string)=>{
+    return `${city} , ${country}`}
+console.log(city_country("lahore","Pakistan"));
+console.log(city_country("Krachi","Pakistan"));
+console.log(city_country("Islamabad","Pakistan"))
+
+//Question Node.40
+
+
+ 
+
+
+//Question no.41
+
+
+//Question no.42
+
+const magicianName:string[]=["cris angle", "shin lim", "dynamo","pen and teller","david copperfield"]
+
+function show_magician(magiciann:string[]):void{
+  for (const magician of magiciann){
+    console.log(magician);}}
+
+
+    function make_great(magician:string[]):string[] {
+      const greatMagicians:string[]=magician.map(magician =>`the great ${magician}`);
+      return greatMagicians;
+    }
+
+    //GreatMagicians
+    const greatmagicianNames:string[]=make_great(magicianName);
+
+    //orginal name
+    show_magician(magicianName);
+
+
+    //the great magican name
+    console.log("\nGreat Magicians:");
+    show_magician(greatmagicianNames)
+
+
+//Question # 43
+const MagicianNames:string[]=["cris angle", "shin lim", "dynamo","pen and teller","david copperfield"]
+
+function showmagicians(magicians:string[]):void{
+  for (const magician of magicians){
+    console.log(magician);}}
+
+
+    function makegreat(magicians:string[]){
+      const greatMagician:string[]=magicians.map(magician=>'the Great ${magician}');
+      return greatMagician;
+    }
+    // Add the great to each magician
+    const greatMagicianNames:string[]=make_great(MagicianNames);
+
+    //orginal name of magicians
+    console.log("original magicians:")
+    showmagicians(MagicianNames)
+
+    //call the function
+    console.log("\nGreat Magicians:");
+    showmagicians(greatMagicianNames);
+
+    //question #44
+    function sandwich(...items:string[]):void{
+      console.log("sanwich summary");
+    if (items.length === 0){
+      console.log(" - you orderwd=ed an empty sndwich.please add some items.")
+    }
+    else{
+      items.forEach((items,i)=>{
+        console.log(` ${i + 1}. ${items}`);
+      });
+    }
+    console.log("\n");}
+
+    //Exercise # 45
+    function creatcar(
+      manufacture:string,model:string,...
+      properties:[string,any][]):any{
+        const car:any ={
+          manufacture,
+          model,
+        };
+        for (const[key,value ]of properties){
+          car[key]=value;
+        }
+        return car;
+      }
+    const mycar=creatcar("toyota","corolla",["red","colour"],[
+      "optionalfeature","sunroof"],["speed","280 KM/hour"]);
+      console.log(mycar);
